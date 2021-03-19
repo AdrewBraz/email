de06d9f40180badd9fd77e58c3dbf28bc9fb21e1
unknown <andbrazhnikov@gmail.com>
2021-03-19 18:40:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,168 @@
+import Imap from 'imap';
+import { inspect } from 'util';
+import {Base64Decode} from 'base64-stream';
+import fs from 'fs'
+import  path from 'path'
+import { result } from 'lodash';
+
+const imap = new Imap({
+    user: 'smp_routes@mail.ru',
+    password: 'CardioSmp',
+    host: 'imap.mail.ru',
+    port: 993,
+    tls: true,
+});
+
+const toUpper = (thing) => thing && thing.toUpperCase ? thing.toUpperCase() : thing;
+
+const cleanEmptyFoldersRecursively = (folder) => {
+    const isDir = fs.statSync(folder).isDirectory();
+    if (!isDir) {
+      return;
+    }
+    const files = fs.readdirSync(folder);
+    if (files.length > 0) {
+      files.forEach((file) => {
+        const fullPath = path.join(folder, file);
+        cleanEmptyFoldersRecursively(fullPath);
+      });
+      files = fs.readdirSync(folder);
+    }
+
+    if (files.length == 0) {
+      console.log("removing: ", folder);
+      fs.rmdirSync(folder);
+      return;
+    }
+  }
+
+
+function findAttachmentParts(struct, attachments) {
+  attachments = attachments ||  [];
+  for (var i = 0, len = struct.length, r; i < len; ++i) {
+    if (Array.isArray(struct[i])) {
+      findAttachmentParts(struct[i], attachments);
+    } else {
+      if (struct[i].disposition && ['INLINE', 'ATTACHMENT'].indexOf(toUpper(struct[i].disposition.type)) > -1) {
+        attachments.push(struct[i]);
+      }
+    }
+  }
+  return attachments;
+}
+
+ function buildAttMessageFunction(attachment, header) {
+  var filename = attachment.params.name.length > 25 ? attachment.params.name.slice(-25, -2) : attachment.params.name
+  var encoding = attachment.encoding;
+  const dirName = (header).replace('Fwd: ', '').replace(/\./g, '')
+  const pathName = `\\\\172.16.11.205\\mc_scans\\СМП\\${dirName}`
+  if(fs.existsSync(pathName)){
+    fs.mkdir(`${pathName}_Повторно`, { recursive: true}, (err) => {
+        if(err) throw err
+    })
+  } else{
+    fs.mkdir(pathName, { recursive: true}, (err) => {
+        if(err) throw err
+    })
+  }
+  return function (msg, seqno) {
+    var prefix = '(#' + seqno + ') ';
+    msg.on('body', function(stream, info) {
+      //Create a write stream so that we can stream the attachment to file;
+      console.log(prefix + 'Streaming this attachment to file', filename, info);
+      var writeStream = fs.createWriteStream(path.resolve(pathName, filename));
+      writeStream.on('finish', function() {
+        console.log(prefix + 'Done writing to file %s', filename);
+      });
+
+      //stream.pipe(writeStream); this would write base64 data to the file.
+      //so we decode during streaming using 
+      if (toUpper(encoding) === 'BASE64') {
+        //the stream is base64 encoded, so here the stream is decode on the fly and piped to the write stream (file)
+        stream.pipe(new Base64Decode()).pipe(writeStream);
+      } else  {
+        //here we have none or some other decoding streamed directly to the file which renders it useless probably
+        stream.pipe(writeStream);
+      }
+    });
+    msg.once('end', function() {
+      console.log(prefix + 'Finished attachment %s', filename);
+    });
+  };
+}
+
+imap.once('ready', function() {
+  imap.openBox('INBOX', false, function(err, box) {
+    console.log(box)
+    if (err) throw err;
+    imap.search(['UNSEEN', ['SINCE', 'January 20, 2021']], (err, result) => {
+        if(err) throw err;
+        console.log(result)
+        var f =  imap.fetch(result, {
+            bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)'],
+            struct: true,
+            markSeen: true
+        });
+        f.on('message', function (msg, seqno) {
+            let header;
+            console.log('Message #%d', seqno);
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                var buffer = '';
+                stream.on('data', function(chunk) {
+                buffer += chunk.toString('utf8');
+                });
+                stream.once('end', function() {
+                console.log(prefix + 'Parsed header: %s', Imap.parseHeader(buffer));
+                header = Imap.parseHeader(buffer).subject[0]
+                });
+            });
+            msg.once('attributes', function(attrs) {
+                var attachments = findAttachmentParts(attrs.struct);
+                const uid = attrs.uid;
+                console.log(prefix + 'Has attachments: %d', attachments.length);
+                for (var i = 0, len=attachments.length ; i < len; ++i) {
+                var attachment = attachments[i];
+                console.log(prefix + 'Fetching attachment %s', attachment.params.name);
+                var f = imap.fetch(attrs.uid , { //do not use imap.seq.fetch here
+                    bodies: [attachment.partID],
+                    struct: true
+                });
+                //build function to process attachment message
+                f.on('message', buildAttMessageFunction(attachment, header));
+                }
+                imap.addFlags(uid, ['\\Seen'], (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Marked as read!")
+                    }
+                })
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished email');
+            });
+        });
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    })
+  });
+});
+
+imap.once('error', function(err) {
+  console.log(err);
+});
+
+imap.once('end', function() {
+  console.log('Connection ended');
+});
+
+imap.connect()
+imap.status();
+
+// cleanEmptyFoldersRecursively('\\\\172.16.11.205\\mc_scans\\Направления на СМП\\')
---
+++ sender.js
@@ -0,0 +1,123 @@
+import Imap from 'imap';
+import { inspect } from 'util';
+import {Base64Decode} from 'base64-stream';
+import fs from 'fs'
+import  path from 'path'
+import { result } from 'lodash';
+import nodemailer from 'nodemailer';
+
+const imap = new Imap({
+    user: 'andyWitman@yandex.ru',
+    password: 'Rem32123',
+    host: 'imap.yandex.ru',
+    port: 993,
+    tls: true,
+});
+
+const main = async (obj) => {
+    const {email, subject} = obj;
+  let transporter = nodemailer.createTransport({
+      host: 'smtp.yandex.ru',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'andyWitman@yandex.ru',
+        pass: 'Rem32123'
+      }
+  })
+
+  let info = await transporter.sendMail({
+      from: 'andyWitman@yandex.ru',
+      to: email,
+      subject: subject,
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>Новый шаблон</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> <!--[if gte mso 9]><xml> <o:OfficeDocumentSettings> <o:AllowPNG></o:AllowPNG> <o:PixelsPerInch>96</o:PixelsPerInch> </o:OfficeDocumentSettings> </xml><![endif]--><style type="text/css">#outlook a {	padding:0;}.es-button {	mso-style-priority:100!important;	text-decoration:none!important;}a[x-apple-data-detectors] {	color:inherit!important;	text-decoration:none!important;	font-size:inherit!important;	font-family:inherit!important;	font-weight:inherit!important;	line-height:inherit!important;}.es-desk-hidden {	display:none;	float:left;	overflow:hidden;	width:0;	max-height:0;	line-height:0;	mso-hide:all;}@media only screen and (max-width:600px) {p, ul li, ol li, a { line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120%!important } h2 { font-size:26px!important; text-align:center; line-height:120%!important } h3 { font-size:20px!important; text-align:center; line-height:120%!important } .es-header-body h1 a, .es-content-body h1 a, .es-footer-body h1 a { font-size:30px!important } .es-header-body h2 a, .es-content-body h2 a, .es-footer-body h2 a { font-size:26px!important } .es-header-body h3 a, .es-content-body h3 a, .es-footer-body h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-content-body p, .es-content-body ul li, .es-content-body ol li, .es-content-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button, button.es-button { font-size:20px!important; display:block!important; border-width:10px 0px 10px 0px!important } .es-adaptive table, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }</style></head>
+<body style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><div class="es-wrapper-color" style="background-color:#F6F6F6"> <!--[if gte mso 9]><v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t"> <v:fill type="tile" color="#f6f6f6"></v:fill> </v:background><![endif]--><table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top"><tr><td valign="top" style="padding:0;Margin:0"><table cellpadding="0" cellspacing="0" class="es-content" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%"><tr><td align="center" style="padding:0;Margin:0"><table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px"><tr><td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px"> <!--[if mso]><table style="width:560px" cellpadding="0" cellspacing="0"><tr><td style="width:180px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left"><tr><td class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;width:180px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="center" style="padding:0;Margin:0;font-size:0px"><a target="_blank" href="https://cardioweb.ru/" style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;text-decoration:underline;color:#2CB543;font-size:14px"><img class="adapt-img" src="https://pcfevf.stripocdn.email/content/guids/CABINET_20026cdb7ce5aed4ada0cf195ac54c05/images/89121616152907869.jpg" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="180" height="180"></a></td>
+</tr></table></td></tr></table> <!--[if mso]></td><td style="width:20px"></td><td style="width:360px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="left" style="padding:0;Margin:0;width:360px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Федеральное государственное бюджетное учреждение<br>НАЦИОНАЛЬНЫЙ МЕДИЦИНСКИЙ<br>ИССЛЕДОВАТЕЛЬСКИЙ ЦЕНТР КАРДИОЛОГИИ<br>Министерства здравоохранения Российской Федерации&nbsp;</p></td>
+</tr></table></td></tr></table> <!--[if mso]></td></tr></table><![endif]--></td>
+</tr><tr><td align="left" style="Margin:0;padding-left:20px;padding-right:20px;padding-top:40px;padding-bottom:40px"><table cellspacing="0" cellpadding="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td class="es-m-p0r" valign="top" align="center" style="padding:0;Margin:0;width:560px"><table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="center" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Добрый день, ваше письмо принято. Спасибо за обращение.<br><br><span style="font-size:16px">Не отвечайте на это письмо!</span></p></td></tr></table></td>
+</tr></table></td>
+</tr><tr><td align="left" style="Margin:0;padding-left:20px;padding-right:20px;padding-top:40px;padding-bottom:40px"><table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="center" valign="top" style="padding:0;Margin:0;width:560px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px"><em></em><em>С уважением, Зиновьева Екатерина Викторовна,<br>руководитель отдела организации оказания<br>медицинской помощи и статистики<br>ФГБУ НМИЦ кардиологии МЗ РФ.</em><em></em><br></p></td>
+</tr></table></td></tr></table></td>
+</tr><tr><td align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px"> <!--[if mso]><table style="width:560px" cellpadding="0" cellspacing="0"><tr><td style="width:270px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left"><tr><td class="es-m-p20b" align="left" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">121552, Москва, ул. 3-я Черепковская, д. 15а</p>
+</td></tr></table></td></tr></table> <!--[if mso]></td><td style="width:20px"></td><td style="width:270px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-right" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right"><tr><td align="left" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333;font-size:14px">Телефон: +7 (495) 150-44-19, 8-800-707-44-19</p></td></tr></table></td></tr></table> <!--[if mso]></td></tr></table><![endif]--></td></tr></table></td>
+</tr></table></td></tr></table></div></body></html>
+`
+  })
+}
+
+imap.once('ready', () => {
+    const arr = [];
+    imap.openBox('INBOX', false, (err, box) => {
+      if (err) throw err;
+      imap.search([ 'UNSEEN', ['SINCE', 'March 19, 2021'] ], function(err, results) {
+        if (err) throw err;
+        console.log(results)
+        var f = imap.fetch(results, { bodies: 'HEADER', struct: true, envelope: true });
+        f.on('message', function(msg, seqno) {
+          console.log('Message #%d', seqno);
+          var prefix = '(#' + seqno + ') ';
+          msg.on('body', function(stream, info) {
+            stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+          });
+          msg.once('attributes', (attrs) => {
+            const { subject, sender } = attrs.envelope;
+            console.log(sender[0])
+            const email = `${sender[0].mailbox}@${sender[0].host}`;
+            arr.push({subject, email});
+          });
+          msg.once('end', function() {
+            console.log(prefix + 'Finished');
+          });
+        });
+        f.once('error', function(err) {
+          console.log('Fetch error: ' + err);
+        });
+        // f.once('end', function() {
+        //   console.log('Done fetching all messages!');
+        //   arr.forEach( item => {
+        //       main(item).catch(console.error)
+        //   })
+        //   imap.end();
+        // });
+      });
+      imap.on('mail', (err, box) => {
+        imap.search(['UNSEEN', ['SINCE', 'March 01, 2021']], (err, results) => {
+            if(err) throw err;
+            console.log(results)
+            var f = imap.fetch(results, { bodies: 'HEADER', struct: true, envelope: true });
+            f.on('message', function(msg, seqno) {
+            console.log('Message #%d', seqno);
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+            });
+            msg.once('attributes', (attrs) => {
+                const { subject, sender } = attrs.envelope;
+                console.log(sender[0])
+                const email = `${sender[0].mailbox}@${sender[0].host}`;
+                arr.push({subject, email});
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished');
+            });
+            });
+            f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+            });
+        })
+    })
+    imap.on('update', (seqno, info) => {
+        console.log(seqno, info)
+    })
+    });
+})
+
+imap.once('error', function(err) {
+    console.log(err);
+  });
+  
+  imap.once('end', function() {
+    console.log('Connection ended');
+  });
+  
+  imap.connect()
