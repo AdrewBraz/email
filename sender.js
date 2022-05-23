3e2b3e07767678f11efb30a6e7f435ba3d36f943
unknown <andbrazhnikov@gmail.com>
2022-05-23 14:36:59 +0300

    'Changes'

--- sender.js
+++ sender.js
@@ -3,11 +3,10 @@ import Imap from 'imap'
 import { castArray, result } from "lodash";
 import nodemailer from 'nodemailer';
 import fs from 'fs'
+import { isEqual } from 'lodash';
 import { promisify } from 'util';
-
-
-
 import excludedMails from './excludedMails';
+import { Console } from 'console';
 
 const imap = new Imap({
     user: 'andyWitman@yandex.ru',
@@ -22,27 +21,30 @@ const imap = new Imap({
     }
 });
 
+const state = {
+  messages: []
+}
 
 const html = fs.readFileSync(`${__dirname}/index.html`, 'utf8')
 
-//   let info = await transporter.sendMail({
-//       from: 'smprknpk@ya.ru',
-//       to: email,
-//       subject: subject,
-//       html
-//   })
-// }
 const openBox = promisify(imap.openBox.bind(imap))
-const fetch = promisify(imap.fetch.bind(imap))
 const search = promisify(imap.search.bind(imap))
-const answerFunc = async (results) => {
-  console.log('smth')
-  const f = await fetch(results, { bodies: 'HEADER', struct: true, envelope: true })
+const fetch = async (results, transporter) => {
+  const f =  imap.fetch(results, { bodies: 'HEADER', struct: true, envelope: true })
   f.on('message', function(msg, seqno) {
     console.log('Message #%d', seqno);
     let prefix = '(#' + seqno + ') ';
-    msg.once('attributes', (attrs) => {
+    msg.once('attributes', async (attrs) => {
       addAttrs(attrs)
+      console.log(shouldBeAnswered(attrs.uid, attrs.flags))
+      // if(shouldBeAnswered(attrs.uid, attrs.flags)){
+      //   await  transporter.sendMail({
+      //     from: 'andyWitman@yandex.ru',
+      //     to: email,
+      //     subject: subject,
+      //     html
+      //   })
+      // }
     });
     msg.once('end', function() {
       console.log(prefix + 'Finished');
@@ -54,13 +56,10 @@ const answerFunc = async (results) => {
   f.on('end', () => {
     console.log('event')
     imap.end()
+    transporter.close()
     imap.destroy()
     console.log('connection closed')
   })
-}
-
-const state = {
-  messages: []
 }
 
 const addAttrs = (attrs) => {
@@ -68,39 +67,39 @@ const addAttrs = (attrs) => {
   const { uid, flags } = attrs;
   const { messages } = state;
   const email = `${sender[0].mailbox}@${sender[0].host}`;
-  if(!excludedMails.includes(email)){
+  if(!excludedMails.includes(email) && !messages.find(el => uid === el.uid)){
     messages.push({uid, email, subject, flags})
     console.log(messages)
   }
   return null
 }
 
+const shouldBeAnswered = (uid, flags) => {
+  const { messages } = state;
+  const mail = messages.find(item => item.uid === uid);
+  const oldFlags = mail.flags
+  console.log( !isEqual(oldFlags, flags))
+  return (!isEqual(oldFlags, flags) && flags.includes('Seen'))
+}
+
 const connectFunc = async () => {
-    const {seqnos, messages} = state;
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.yandex.ru',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'andyWitman@yandex.ru',
+      pass: 'Rem32123'
+    }
+  })
     console.log('open box')
     await openBox('INBOX', false)
       .then(box => console.log(box))
       .catch(err => console.log(err))
-    await search([ 'UNSEEN', ['SINCE', 'Apr 01, 2021'] ])
+    await search([ 'ALL', ['SINCE', 'Apr 01, 2021'] ])
       .then((results) => {
         if(results.length > 0){
-          const f =  imap.fetch(results, { bodies: 'HEADER', struct: true, envelope: true })
-          f.on('message', function(msg, seqno) {
-            console.log('Message #%d', seqno);
-            let prefix = '(#' + seqno + ') ';
-            msg.once('attributes', (attrs) => {
-              addAttrs(attrs)
-            });
-            msg.once('end', function() {
-              console.log(prefix + 'Finished');
-            });
-          });
-          f.once('error', function(err) {
-            console.log('Fetch error: ' + err);
-          })
-          f.on('end', () => {
-            console.log('stream closed')
-          })
+          fetch(results, transporter)
         }
       })
       .catch(err => console.log(err))
@@ -112,13 +111,6 @@ const main = () => {
   imap.once('ready', connectFunc)
   imap.connect()
 }
-
-// console.log('start')
-//   imap.once('ready', connectFunc)
-//   imap.once('error', function(err) {
-//     console.log(err + '  watta fuck');
-//   });
-//   imap.connect()
 
 const job = new CronJob(
   '0 */1 * * * *',
